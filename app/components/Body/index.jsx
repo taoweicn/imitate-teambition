e328dc089b27387881dba7cbc4204f5672b642f7
taoweicn <twchn@live.com>
2017-11-16 17:33:14 +0800

    Complete Menu

--- app/components/Body/index.jsx
+++ app/components/Body/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import style from './style.scss';
+import Menu from '../Menu';
 
 export default function Body() {
   return (
-    <main className={style.main} />
+    <main className={style.main}>
+      <Menu />
+    </main>
   );
 }
